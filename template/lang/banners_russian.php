6f6e17a13031289b5efa4c1344715998a0caf61f
Alexander <alexander.albrant@gmail.com>
2025-04-15 23:08:45 +0200

    add russian translation for template
    translated files
    admin/mail
    mail
    land

---
+++ template/lang/banners_russian.php
@@ -0,0 +1,30 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: banners_english.php 15291 2023-07-06 11:46:25Z GTB $
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+   define('TEXT_BANNER_GROUP_FOR_TEMPLATE', 'Группы баннеров для вашего шаблона');
+   define('TEXT_RECOMMENDED_BANNER_SETTINGS', 'Рекомендуемые настройки баннеров для');
+   define('TEXT_CONFIG_IMAGE_OPTIONS', 'Конфигурация -> Параметры изображений');
+   define('TEXT_BANNER_IMAGES_WIDTH', 'Ширина баннеров:');
+   define('TEXT_BANNER_IMAGES_HEIGHT', 'Высота баннеров:');
+   define('TEXT_BANNER_IMAGES_WIDTH_MOBILE', 'Ширина баннеров (мобильные):');
+   define('TEXT_BANNER_IMAGES_HEIGHT_MOBILE', 'Высота баннеров (мобильные):');
+   define('TEXT_SLIDER', 'Слайдер');
+   define('TEXT_SLIDER_BANNER', 'Слайдер / Баннер');
+   define('TEXT_BANNER_GROUP', 'Группа баннеров:');
+   define('TEXT_WIDTH', 'Ширина');
+   define('TEXT_DESKTOP', 'Десктоп:');
+   define('TEXT_MOBILE', 'Мобильный:');
+   define('TEXT_BANNER_ROW_1', 'Баннер, ряд 1');  
+   define('TEXT_BANNER_ROW_2', 'Баннер, ряд 2');  
+   define('TEXT_BANNER_ROW_3', 'Баннер, ряд 3');
+   define('TEXT_BANNER_ROW_4', 'Баннер, ряд 4');
+   
